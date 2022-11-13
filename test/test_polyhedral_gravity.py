86365c42f1916d6073642175769b8f73a9194b4a
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2022-11-13 19:56:57 +0100

    add test cases

--- test/test_polyhedral_gravity.py
+++ test/test_polyhedral_gravity.py
@@ -1,9 +1,9 @@
 import pytest
 import pickle as pk
 import torch
+import numpy as np
 import gravann._polyhedral_labels as polyhedral_labels
 import gravann._mascon_labels as mascons_labels
-
 
 EROS_LP_FILE_NAME = './3dmeshes/eros.pk'
 EROS_MASCON_FILE_NAME = './mascons/eros.pk'
@@ -22,8 +22,14 @@ def mesh_data(filename):
         return pk.load(f)
 
 
-def test_eros_potential():
-    target_points = [[1.0, 1.0, 1.0]]
+@pytest.fixture()
+def setup():
+    torch.set_printoptions(precision=14)
+
+
+def test_eros_far(setup):
+    coordinates = np.array([-100.0, 100.0])
+    target_points = np.array(np.meshgrid(coordinates, coordinates, coordinates)).T.reshape(-1, 3)
     target_points_tensor = torch.tensor(target_points)
 
     # Inverse of Gravity Constant 6.6743015×10−11
@@ -32,15 +38,24 @@ def test_eros_potential():
     mascon_points, mascon_masses = mascon_data(EROS_MASCON_FILE_NAME)
     vertices, triangles = mesh_data(EROS_LP_FILE_NAME)
 
-    mascon_potential = mascons_labels.U_L(target_points_tensor, mascon_points, mascon_masses)
-    polyhedral_potential = polyhedral_labels.U_L(target_points, vertices, triangles, density)
+    mascon_potential = torch.squeeze(mascons_labels.U_L(target_points_tensor, mascon_points, mascon_masses))
+    polyhedral_potential = torch.squeeze(polyhedral_labels.U_L(target_points, vertices, triangles, density))
 
-    assert mascon_potential == polyhedral_potential, \
+    scaling_factor = mascon_potential[0] / polyhedral_potential[0]
+
+    mascon_acceleration = torch.squeeze(mascons_labels.ACC_L(target_points_tensor, mascon_points, mascon_masses))
+    polyhedral_acceleration = torch.squeeze(polyhedral_labels.ACC_L(target_points, vertices, triangles, density))
+
+    assert mascon_potential == pytest.approx(polyhedral_potential * scaling_factor, abs=1e-8), \
         f"Mascon {mascon_potential}; Polyhedral {polyhedral_potential}"
 
+    assert mascon_acceleration == pytest.approx(polyhedral_acceleration * scaling_factor, abs=1e-8), \
+        f"Mascon {mascon_acceleration}; Polyhedral {polyhedral_acceleration}"
 
-def test_eros_acceleration():
-    target_points = [[1.0, 1.0, 1.0]]
+
+def test_eros_mid(setup):
+    coordinates = np.array([-10.0, 10.0])
+    target_points = np.array(np.meshgrid(coordinates, coordinates, coordinates)).T.reshape(-1, 3)
     target_points_tensor = torch.tensor(target_points)
 
     # Inverse of Gravity Constant 6.6743015×10−11
@@ -49,8 +64,41 @@ def test_eros_acceleration():
     mascon_points, mascon_masses = mascon_data(EROS_MASCON_FILE_NAME)
     vertices, triangles = mesh_data(EROS_LP_FILE_NAME)
 
-    mascon_acceleration = mascons_labels.ACC_L(target_points_tensor, mascon_points, mascon_masses)
-    polyhedral_acceleration = polyhedral_labels.ACC_L(target_points, vertices, triangles, density)
+    mascon_potential = torch.squeeze(mascons_labels.U_L(target_points_tensor, mascon_points, mascon_masses))
+    polyhedral_potential = torch.squeeze(polyhedral_labels.U_L(target_points, vertices, triangles, density))
 
-    assert all([m == p for m, p in zip(mascon_acceleration[0], polyhedral_acceleration[0])]), \
-        f"Mascon {mascon_acceleration[0]}; Polyhedral {polyhedral_acceleration[0]}"
+    scaling_factor = mascon_potential[0] / polyhedral_potential[0]
+
+    mascon_acceleration = torch.squeeze(mascons_labels.ACC_L(target_points_tensor, mascon_points, mascon_masses))
+    polyhedral_acceleration = torch.squeeze(polyhedral_labels.ACC_L(target_points, vertices, triangles, density))
+
+    assert mascon_potential == pytest.approx(polyhedral_potential * scaling_factor, abs=1e-8), \
+        f"Mascon {mascon_potential}; Polyhedral {polyhedral_potential}"
+
+    assert mascon_acceleration == pytest.approx(polyhedral_acceleration * scaling_factor, abs=1e-8), \
+        f"Mascon {mascon_acceleration}; Polyhedral {polyhedral_acceleration}"
+
+def test_eros_near(setup):
+    coordinates = np.array([-1.0, 1.0])
+    target_points = np.array(np.meshgrid(coordinates, coordinates, coordinates)).T.reshape(-1, 3)
+    target_points_tensor = torch.tensor(target_points)
+
+    # Inverse of Gravity Constant 6.6743015×10−11
+    density = 1.49828e10
+
+    mascon_points, mascon_masses = mascon_data(EROS_MASCON_FILE_NAME)
+    vertices, triangles = mesh_data(EROS_LP_FILE_NAME)
+
+    mascon_potential = torch.squeeze(mascons_labels.U_L(target_points_tensor, mascon_points, mascon_masses))
+    polyhedral_potential = torch.squeeze(polyhedral_labels.U_L(target_points, vertices, triangles, density))
+
+    scaling_factor = mascon_potential[0] / polyhedral_potential[0]
+
+    mascon_acceleration = torch.squeeze(mascons_labels.ACC_L(target_points_tensor, mascon_points, mascon_masses))
+    polyhedral_acceleration = torch.squeeze(polyhedral_labels.ACC_L(target_points, vertices, triangles, density))
+
+    assert mascon_potential == pytest.approx(polyhedral_potential * scaling_factor, abs=1e-5), \
+        f"Mascon {mascon_potential}; Polyhedral {polyhedral_potential}"
+
+    assert mascon_acceleration == pytest.approx(polyhedral_acceleration * scaling_factor, abs=1e-5), \
+        f"Mascon {mascon_acceleration}; Polyhedral {polyhedral_acceleration}"
