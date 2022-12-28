26dbda34fc999ebd86d02cc31dc9ac0936f8b8e8
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2022-12-28 23:11:03 +0100

    new tests for volume calculation, refactored tests to use relative error and to use the actual density instead of an approximated one

--- test/test_polyhedral_gravity.py
+++ test/test_polyhedral_gravity.py
@@ -1,10 +1,12 @@
-import pytest
-import pickle as pk
-import torch
-import numpy as np
 import itertools
-import gravann
-import gravann.polyhedral
+
+import numpy as np
+import pytest
+import torch
+
+from gravann import ACC_L as MASCON_ACC_L, U_L as MASCON_U_L, load_mascon_data, load_polyhedral_mesh
+from gravann.polyhedral import ACC_L as POLYHEDRAL_ACC_L, U_L as POLYHEDRAL_U_L, calculate_density, \
+    GRAVITY_CONSTANT_INVERSE
 
 # ====================== TEST PARAMETERS ======================
 # The tested bodies
@@ -12,77 +14,77 @@ TEST_FILENAMES = ["bennu", "churyumov-gerasimenko", "eros", "hollow", "itokawa",
 # The tested distances to the body center
 TEST_DISTANCES = [1.0, 5.0, 1e2, 5e2, 1e3]
 # The test epsilon needed for comparison between mascon & polyhedral model
-TEST_EPSILON = 1e-1
+TEST_EPSILON = 1e-3
 # Exclude certain bodies with distance combinations from failing with the currently set TEST_EPSILON
 TEST_EXCLUDE = {
+    "churyumov-gerasimenko": [1000],
+    "eros": [1000],
+    "hollow": [1],
+    "itokawa": [1000],
+    "planetesimal": [1, 5],
+    "torus": [1000]
 }
-# ====================== TEST PARAMETERS ======================
 
 
-# Inverse of Gravity Constant 6.6743015×10−11
-GRAVITY_CONSTANT_INVERSE = 1.49828e10
+# ======================= SETUP UTILITY =======================
+def get_data(sample: str):
+    """
+    Utility function. Gets the parameters for a sample body
+    Args:
+        sample: the name of the sample
 
+    Returns:
+        tuple of mascon_points, mascon_masses, vertices, triangle_faces, density, the name of the sample
 
-def get_scaling_factor(mascon_points, mascon_masses, vertices, triangles):
-    # Generate the input
-    coordinates = np.array([-1.0, 1.0])
-    cartesian_points = np.array(np.meshgrid(coordinates, coordinates, coordinates)).T.reshape(-1, 3)
-    cartesian_points_tensor = torch.tensor(cartesian_points)
-    # Evaluate the models
-    mascon_potential = torch.squeeze(gravann.U_L(cartesian_points_tensor, mascon_points, mascon_masses))
-    polyhedral_potential = torch.squeeze(
-        gravann.polyhedral.U_L(cartesian_points_tensor, vertices, triangles, GRAVITY_CONSTANT_INVERSE))
-    # Compute the scaling factor as average around our normed body
-    return torch.mean(mascon_potential / polyhedral_potential)
+    """
+    # Get the mascon & mesh data
+    mascon_points, mascon_masses = load_mascon_data(sample)
+    vertices, triangles = load_polyhedral_mesh(sample)
 
-
-def get_data(file_name):
-    # Read the mascon & mesh data
-    mascon_points, mascon_masses = gravann.load_mascon_data(file_name)
-    vertices, triangles = gravann.load_polyhedral_mesh(file_name)
-
-    # Compute the scaling factor as average around our normed body
-    scaling_factor = get_scaling_factor(mascon_points, mascon_masses, vertices, triangles)
+    # Calculate the density as required for the polyhedral gravity model
+    density = calculate_density(vertices, triangles)
 
     # Pack everything together
-    return (mascon_points, mascon_masses), (vertices, triangles), scaling_factor, file_name
+    return (mascon_points, mascon_masses), (vertices, triangles), density, sample
 
 
 # The Data produced by get_data
 TEST_DATA = [get_data(file_name) for file_name in TEST_FILENAMES]
-
 # The parameters combined as test input data & the test distances
 TEST_PARAMETERS = list(itertools.product(TEST_DATA, TEST_DISTANCES))
-
 # More readable names for the parametrized tests
 TEST_NAMES = [f"{name}-{distance}" for name, distance in itertools.product(TEST_FILENAMES, TEST_DISTANCES)]
 
 
+# ======================== TEST CASE ==========================
 @pytest.mark.parametrize("data, distance", TEST_PARAMETERS, ids=TEST_NAMES)
 def test_compare_mascon_polyhedral_model(data, distance):
-    mascon_data, mesh_data, scaling_factor, body_name = data
+    mascon_data, mesh_data, density, body_name = data
     # Set the print precision of torch for more reasonable messages
     torch.set_printoptions(precision=20)
     # Get the Mascon Parameters
     mascon_points, mascon_masses = mascon_data
-    assert torch.sum(mascon_masses) == pytest.approx(1.0)
     # Get the Mesh Parameters
     vertices, triangles = mesh_data
 
-    # Get a normed density for the polyhedral model (set G to 1 via its inverse * a scaling_factor)
-    # The scaling factor is derived via the difference of mascon & polyhedral model
-    density = GRAVITY_CONSTANT_INVERSE * scaling_factor
+    # First assert that the mass of the mascon is actually normed and equals one (1.0)
+    assert torch.sum(mascon_masses) == pytest.approx(1.0)
+
+    # This is just a scaling factor: The solved triple integral is multiplied by the density and the gravity constant
+    # In order to set the gravity constant G = 1, we give its inverse to polyhedral model
+    polyhedral_gravity_factor = GRAVITY_CONSTANT_INVERSE * density
 
     coordinates = np.array([-distance, distance])
     cartesian_points = np.array(np.meshgrid(coordinates, coordinates, coordinates)).T.reshape(-1, 3)
     cartesian_points_tensor = torch.tensor(cartesian_points)
 
     # Compute the potential and the acceleration with the two model
-    mascon_potential = torch.squeeze(gravann.U_L(cartesian_points_tensor, mascon_points, mascon_masses))
-    polyhedral_potential = torch.squeeze(gravann.polyhedral.U_L(cartesian_points_tensor, vertices, triangles, density))
-    mascon_acceleration = torch.squeeze(gravann.ACC_L(cartesian_points_tensor, mascon_points, mascon_masses))
+    mascon_potential = torch.squeeze(MASCON_U_L(cartesian_points_tensor, mascon_points, mascon_masses))
+    polyhedral_potential = torch.squeeze(
+        POLYHEDRAL_U_L(cartesian_points_tensor, vertices, triangles, polyhedral_gravity_factor)) * -1.0
+    mascon_acceleration = torch.squeeze(MASCON_ACC_L(cartesian_points_tensor, mascon_points, mascon_masses))
     polyhedral_acceleration = torch.squeeze(
-        gravann.polyhedral.ACC_L(cartesian_points_tensor, vertices, triangles, density))
+        POLYHEDRAL_ACC_L(cartesian_points_tensor, vertices, triangles, polyhedral_gravity_factor)) * -1.0
 
     # Compare the results
     if not (body_name in TEST_EXCLUDE and distance in TEST_EXCLUDE[body_name]):
--- test/test_polyhedral_utils.py
+++ test/test_polyhedral_utils.py
@@ -1,7 +1,6 @@
-import pytest
-
 import numpy as np
-from gravann.polyhedral import calculate_volume, calculate_density
+
+from gravann.polyhedral import calculate_volume
 
 cube_vertices = np.array([
     [-1, -1, -1],
@@ -29,7 +28,24 @@ cube_faces = np.array([
     [4, 6, 7]
 ])
 
+tetraeder_vertices = np.array([
+    [-0.5, 0, 0],
+    [0.5, 0, 0],
+    [0, 0.86602540378443864676372317075, 0],
+    [0, 0.28867513459481288225457439025, 0.8164965809277260327324280249019]
+])
+
+tetraeder_faces = np.array([
+    [0, 2, 1],
+    [0, 1, 3],
+    [0, 3, 2],
+    [1, 2, 3]
+])
+
 
 def test_cube_volume():
     assert 8 == calculate_volume(cube_vertices, cube_faces)
 
+
+def test_tetraeder_volume():
+    assert 0.1178511301977579 == calculate_volume(tetraeder_vertices, tetraeder_faces)
