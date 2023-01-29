9cd278f839a94cc963d3ca1c279dc278ed58f65c
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2023-01-29 16:33:12 +0100

    comments and small changes for training toolchain

--- gravann/_train_v2.py
+++ gravann/_train_v2.py
@@ -8,7 +8,16 @@ from ._train import init_network
 from ._utils import fixRandomSeeds, EarlyStopping
 
 
-def _init_environment(parameters: {str, any}):
+def _init_environment(parameters: {str, any}) -> str:
+    """Creates the environment (the run-folder) for the given training with parameters
+
+    Args:
+        parameters: dictionary of parameters of this training run
+
+    Returns:
+        the run folder
+
+    """
     # Clear GPU memory
     torch.cuda.empty_cache()
 
@@ -21,6 +30,7 @@ def _init_environment(parameters: {str, any}):
     # Create folder for this specific run
     run_folder = f"""
         {parameters['output_folder']}/
+        {parameters['method']}/
         {parameters['sample']}/
         LR={parameters['learning_rate']}_loss={parameters['parameters'].__name__}_ENC={parameters['encoding'].name}_
         BS={parameters['batch_size']}_layers={parameters['hidden_layers']}_neurons={parameters['n_neurons']}_
@@ -32,6 +42,22 @@ def _init_environment(parameters: {str, any}):
 
 def _init_model(run_folder: str, encoding: any, n_neurons: int, activation: any, model_type: str, omega: float,
                 hidden_layers: int, learning_rate: float):
+    """Initializes the model and the associated training utility
+
+    Args:
+        run_folder: the folder where to store the model in case of early stopping
+        encoding: encoding instance to use for the network
+        n_neurons: the number of neurons per layer
+        activation: activation function for the last network layer
+        model_type: the model type
+        omega: Omega value for siren activations
+        hidden_layers: the number of hidden layer
+        learning_rate: the utilized learning rate for the optimizer
+
+    Returns:
+        Tuple of model, the early_stopper, optimizer, scheduler
+
+    """
     # Initializes the model
     model = init_network(
         encoding,
@@ -62,6 +88,18 @@ def _init_model(run_folder: str, encoding: any, n_neurons: int, activation: any,
 
 
 def _init_training_sampler(sample: str, target_sample_method: str, sample_domain: [float], batch_size: int):
+    """Creates a new target point sample with the given method and sample domain.
+
+    Args:
+        sample: the sample body's name
+        target_sample_method: the sample method (e.g. 'spherical')
+        sample_domain: the sample domain, sspecifies the sampling radius.
+        batch_size: the number of points per function call
+
+    Returns:
+        sampling function
+
+    """
     return get_target_point_sampler(
         batch_size,
         method=target_sample_method,
@@ -70,49 +108,36 @@ def _init_training_sampler(sample: str, target_sample_method: str, sample_domain
     )
 
 
-def _train_on_batch_v2(targets, labels, model, encoding, loss_fn, optimizer, scheduler, integrator, N,
-                       integration_domain=None):
+def _train_on_batch_v2(points, model_fn, labels, loss_fn, optimizer, scheduler):
     """Trains the passed model on the passed batch
 
     Args:
-        targets (tensor): target points for training
+        points (tensor): target points for training
+        model_fn (func): prediction func of the model
         labels (tensor): labels at the target points
-        model (torch model): model to train
-        encoding (func): encoding function for the model
         loss_fn (func): loss function for training
         optimizer (torch optimizer): torch optimizer to use
         scheduler (torch LR scheduler): torch LR scheduler to use
-        integrator (func): integration function to call for the training loss
-        N (int): Number of integration points to use for training
-        integration_domain (torch.tensor): Domain to pick integration points in, only works with trapezoid for now
 
     Returns:
         torch tensor: losses
     """
     # Compute the loss (use N=3000 to start with, then, eventually, beef it up to 200000)
-    predicted = integrator(targets, model, encoding, N=N, domain=integration_domain)
+    # predicted = integrator(points, model, encoding, N=N, domain=integration_domain)
+    predicted = model_fn(points)
     c = torch.sum(predicted * labels) / torch.sum(predicted * predicted)
-    if loss_fn == contrastive_loss or loss_fn == normalized_relative_L2_loss or loss_fn == normalized_relative_component_loss:
+
+    if loss_fn in [contrastive_loss, normalized_relative_L2_loss, normalized_relative_component_loss]:
         loss = loss_fn(predicted, labels)
     else:
         loss = loss_fn(predicted.view(-1), labels.view(-1))
 
-    # Before the backward pass, use the optimizer object to zero all of the
-    # gradients for the variables it will update (which are the learnable
-    # weights of the model). This is because by default, gradients are
-    # accumulated in buffers( i.e, not overwritten) whenever .backward()
-    # is called. Checkout docs of torch.autograd.backward for more details.
     optimizer.zero_grad()
 
-    # Backward pass: compute gradient of the loss with respect to model
-    # parameters
     loss.backward()
 
-    # Calling the step function on an Optimizer makes an update to its
-    # parameters
     optimizer.step()
 
-    # Perform a step in LR scheduler to update LR
     scheduler.step(loss.item())
 
     return loss, c
