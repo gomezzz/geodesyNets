3069774f1c420447e8c00806c1b43c5cc2319496
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2023-01-29 13:59:55 +0100

    move compute_c (kappa) in its own file

--- gravann/__init__.py
+++ gravann/__init__.py
@@ -6,49 +6,42 @@ import os
 
 # Importing encodings for the spacial asteroid dimensions
 from ._encodings import directional_encoding, positional_encoding, direct_encoding, spherical_coordinates
-
-# Importing the losses
-from ._losses import normalized_loss, mse_loss, normalized_L1_loss, contrastive_loss, normalized_sqrt_L1_loss, normalized_relative_L2_loss, normalized_relative_component_loss
-
+# Importing misc methods for 3D graphics
+from ._hulls import alpha_shape, ray_triangle_intersect, rays_triangle_intersect, is_outside, is_inside, \
+    is_outside_torch
 # Importing the method to integrate the density rho(x,y,z) output of an ANN in the unit cube
 from ._integration import ACC_ld, ACC_trap, U_mc, U_ld, U_trap_opt, rho_trap
 from ._integration import compute_sobol_points, compute_integration_grid
-
 # Methods to load mascons etc.
 from ._io import load_sample, load_polyhedral_mesh, load_mascon_data
-
-# Importing misc methods for 3D graphics
-from ._hulls import alpha_shape, ray_triangle_intersect, rays_triangle_intersect, is_outside, is_inside, is_outside_torch
-
+# Computation of the constant c (kappa in the paper)
+from ._kappa import compute_c_for_model, compute_c_for_model_v2
+# Importing the losses
+from ._losses import normalized_loss, mse_loss, normalized_L1_loss, contrastive_loss, normalized_sqrt_L1_loss, \
+    normalized_relative_L2_loss, normalized_relative_component_loss
+# Import the labeling functions the mascons
+from ._mascon_labels import U_L, ACC_L, ACC_L_differential
+# Importing the mesh_conversion methods
+from ._mesh_conversion import create_mesh_from_cloud, create_mesh_from_model
 # Importing the plots
 from ._plots import plot_mascon, plot_model_grid, plot_model_rejection, plot_model_contours, plot_potential_contours
 from ._plots import plot_mesh, plot_model_mesh, plot_point_cloud_mesh, plot_points, plot_model_mascon_acceleration
-from ._plots import plot_model_vs_cloud_mesh, plot_gradients_per_layer, plot_model_vs_mascon_rejection, plot_model_vs_mascon_contours
-
-# Importing the validation method
-from ._validation import validation, validation_v2, validation_results_unpack_df, compute_c_for_model, compute_c_for_model_v2
-from ._validation_mascon import validation_mascon
-
+from ._plots import plot_model_vs_cloud_mesh, plot_gradients_per_layer, plot_model_vs_mascon_rejection, \
+    plot_model_vs_mascon_contours
 # Importing methods to sample points around asteroid
 from ._sample_observation_points import get_target_point_sampler
-
-# Importing the mesh_conversion methods
-from ._mesh_conversion import create_mesh_from_cloud, create_mesh_from_model
-
-# Import the labeling functions the mascons
-from ._mascon_labels import U_L, ACC_L, ACC_L_differential
-
-# Import training utility functions
-from ._train import init_network, train_on_batch, run_training, load_model_run
-
-# Custom layer for siren
-from .networks._abs_layer import AbsLayer
-
-# Import utility functions
-from ._utils import max_min_distance, enableCUDA, fixRandomSeeds, print_torch_mem_footprint, get_asteroid_bounding_box, EarlyStopping, unpack_triangle_mesh
-
 # Stokes coefficient utilities
 from ._stokes import mascon2stokes, Clm, Slm, constant_factors, legendre_factory_torch
+# Import training utility functions
+from ._train import init_network, train_on_batch, run_training, load_model_run
+# Import utility functions
+from ._utils import max_min_distance, enableCUDA, fixRandomSeeds, print_torch_mem_footprint, get_asteroid_bounding_box, \
+    EarlyStopping, unpack_triangle_mesh
+# Importing the validation method
+from ._validation import validation, validation_v2, validation_results_unpack_df
+from ._validation_mascon import validation_mascon
+# Custom layer for siren
+from .networks._abs_layer import AbsLayer
 
 # Set main device by default to cpu if no other choice was made before
 if "TORCH_DEVICE" not in os.environ:
---
+++ gravann/_kappa.py
@@ -0,0 +1,111 @@
+import torch
+
+from gravann.polyhedral import ACC_L as POLYHEDRAL_ACC_L, U_L as POLYHEDRAL_U_L
+from ._integration import ACC_trap, U_trap_opt
+from ._io import load_sample, load_polyhedral_mesh
+from ._mascon_labels import ACC_L as MASCON_ACC_L, U_L as MASCON_U_L, ACC_L_differential as MASCON_ACC_L_differential
+from ._sample_observation_points import get_target_point_sampler
+
+
+def compute_c_for_model_v2(model, encoding, method, use_acc=True, **kwargs):
+    """Convenience function to calculate the current c constant for a model (based on mascon or polyhedral mesh data)
+
+    Args:
+        model (torch.nn): trained model
+        encoding (encoding): encoding to use for the points
+        method (str): either 'mascon' or 'polyhedral'
+        use_acc (bool): if acceleration should be used (otherwise potential)
+
+
+    Keyword Args:
+        sample (str): sample name (file name) instead of mascons or mesh data
+        mascon_points (torch.tensor): asteroid mascon points
+        mascon_masses (torch.tensor): asteroid mascon masses
+        mascon_masses_nu (torch.tensor): asteroid mascon masses
+        mesh_vertices ((N, 3) array-like): the vertices of a polyhedron
+        mesh_faces ((N, 3) array-like): the triangular faces of a polyhedron
+        density (float): the density of a polyhedron
+    """
+    sample = kwargs.get("sample", None)
+    if method == 'mascon':
+        if sample is not None:
+            mascon_points, mascon_masses, mascon_masses_nu = load_sample(sample, use_acc)
+        else:
+            mascon_points, mascon_masses, mascon_masses_nu = kwargs.get("mascon_points", None), kwargs.get(
+                "mascon_masses", None), kwargs.get("mascon_masses_nu", None)
+        return compute_c_for_model(model, encoding, mascon_points, mascon_masses, mascon_masses_nu, use_acc)
+    elif method == 'polyhedral':
+        if sample is not None:
+            mesh_vertices, mesh_faces = load_polyhedral_mesh(sample)
+        else:
+            mesh_vertices, mesh_faces = kwargs.get("mesh_vertices", None), kwargs.get("mesh_faces", None)
+        density = kwargs.get("density", 1.0)
+        return _compute_c_for_model_polyhedral(model, encoding, mesh_vertices, mesh_faces, density, use_acc)
+    else:
+        raise NotImplemented(f"The method {method} is not implemented for compute_c!")
+
+
+def compute_c_for_model(model, encoding, mascon_points, mascon_masses, mascon_masses_nu=None, use_acc=True):
+    """Computes the current c constant for a model (given mascon points)
+
+    Args:
+        model (torch.nn): trained model
+        encoding (encoding): encoding to use for the points
+        mascon_points (torch.tensor): asteroid mascon points
+        mascon_masses (torch.tensor): asteroid mascon masses
+        mascon_masses_nu (torch.tensor): asteroid mascon masses
+        use_acc (bool): if acceleration should be used (otherwise potential)
+    """
+    if use_acc:
+        if mascon_masses_nu is None:
+            return _compute_c_for_model(
+                lambda x: MASCON_ACC_L(x, mascon_points, mascon_masses),
+                lambda x: ACC_trap(x, model, encoding, N=100000)
+            )
+        else:
+            return _compute_c_for_model(
+                lambda x: MASCON_ACC_L_differential(x, mascon_points, mascon_masses, mascon_masses_nu),
+                lambda x: ACC_trap(x, model, encoding, N=100000)
+            )
+    else:
+        return _compute_c_for_model(
+            lambda x: MASCON_U_L(x, mascon_points, mascon_masses),
+            lambda x: U_trap_opt(x, model, encoding, N=100000)
+        )
+
+
+def _compute_c_for_model_polyhedral(model, encoding, mesh_vertices, mesh_faces, density, use_acc=True):
+    """Computes the current c constant for a model.
+
+    Args:
+        model (torch.nn): trained model
+        encoding (encoding): encoding to use for the points
+        mesh_vertices ((N, 3) array): mesh vertices
+        mesh_faces ((M, 3) array): mesh triangles
+        density (Float): the density of the polyhedron
+        use_acc (bool): if acceleration should be used (otherwise potential)
+    """
+    if use_acc:
+        return _compute_c_for_model(
+            lambda x: POLYHEDRAL_ACC_L(x, mesh_vertices, mesh_faces, density),
+            lambda x: ACC_trap(x, model, encoding, N=100000)
+        )
+    else:
+        return _compute_c_for_model(
+            lambda x: POLYHEDRAL_U_L(x, mesh_vertices, mesh_faces, density),
+            lambda x: U_trap_opt(x, model, encoding, N=100000)
+        )
+
+
+def _compute_c_for_model(label, label_trap):
+    """Common method computing the current c constant for a model.
+
+    Args:
+        label: mascon or polyhedral label (only takes target points as argument)
+        label_trap : label of the trained model (only takes target points as argument)
+    """
+    targets_point_sampler = get_target_point_sampler(1000, method="spherical", bounds=[0.81, 1.0])
+    target_points = targets_point_sampler()
+    labels = label(target_points)
+    predicted = label_trap(target_points)
+    return (torch.sum(predicted * labels) / torch.sum(predicted * predicted)).item()
