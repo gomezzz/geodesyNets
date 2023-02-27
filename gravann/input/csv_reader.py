261c3cc74ee27c147d2dab321b02105a86066808
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2023-02-27 20:38:34 +0100

    makes csv reader great to use

--- gravann/input/csv_reader.py
+++ gravann/input/csv_reader.py
@@ -1,13 +1,32 @@
 import os
+from typing import Union, List
 
 import pandas as pd
 
 
-def read_result_csv(root_directory, dir_substring="") -> pd.DataFrame:
+def read_result_csv(
+        root_directory: os.PathLike,
+        include: Union[List[str], None] = None,
+        exclude: Union[List[str], None] = None
+) -> pd.DataFrame:
+    """This method reads all csv files in a given root directory, where the sub-path contains a specific pattern.
+
+    Args:
+        root_directory: the root directory from which to recursively search for csv files
+        include: the patterns the sub-path must contain
+        exclude: the patterns the sub-paths is not allowed to contain
+
+    Returns:
+        pandas DataFrame of concatenated csv files
+
+    """
     dataframe = pd.DataFrame()
-    for dirpath, _, filenames in os.walk(root_directory):
+    for dir_path, _, filenames in os.walk(root_directory):
         for filename in filenames:
-            if str(filename) == "results.csv" and dir_substring in str(dirpath):
-                element = pd.read_csv(os.path.join(dirpath, filename))
+            if not filename.endswith(".csv"):
+                continue
+            path = os.path.join(dir_path, filename)
+            if any(x in path for x in include) and not any(x in path for x in exclude):
+                element = pd.read_csv(path)
                 dataframe = pd.concat([dataframe, element])
     return dataframe
