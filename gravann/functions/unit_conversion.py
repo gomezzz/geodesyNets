46d54296bd7fcf2e96285904bb519f7570626ec1
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2023-03-01 23:06:28 +0100

    implements unit conversion

--- gravann/functions/unit_conversion.py
+++ gravann/functions/unit_conversion.py
@@ -1,3 +1,6 @@
+import re
+from typing import List
+
 import numpy as np
 import pandas as pd
 from numpy.typing import ArrayLike
@@ -17,23 +20,50 @@ CONVERSION_FACTORS = {
 }
 
 
-def convert_pandas_altitudes(result_df: pd.DataFrame, altitudes) -> pd.DataFrame:
+def convert_pandas_altitudes(
+        result_df: pd.DataFrame, altitudes: List[float], convert_height: bool = False, only_height: bool = False,
+) -> pd.DataFrame:
+    """Converts the entries of the given pandas DataFrame. Operates in-place!
+
+    Args:
+        result_df: the result dataframe for the validation & training containing all information
+        altitudes: the altitudes used in the configuration as extra-validation heights, e.g. [0.25, 0.5, 1.0]
+        convert_height: if True, replace the normalized heights with the real heights, e.g. 0.05 --> 0.31224...
+        only_height: if True, replace the names with only the height, e.g. relRMSE@0.05 --> only 0.05
+            (can unambiguous in the end!)
+
+
+    Returns:
+        pandas DataFrame
+
+    """
     # Legacy stuff
     if "Sample" in result_df:
         result_df.rename(columns={"Sample": "sample"}, inplace=True)
     if "Target Sampler Domain" in result_df:
         result_df.rename(columns={"Target Sampler Domain": "sample_domain"}, inplace=True)
 
+    # Iterate over all possible samples
     for sample_name, (altitude2metric, value2metric) in CONVERSION_FACTORS.items():
-        # np.fromstring("[1.0, 5.0]".replace('[', '').replace(']', ''), sep=',')
         # First adapt validation results
         for column in [x for x in result_df.columns if x.lower().startswith(("Normalized L1 Loss", "RMSE", "relRMSE"))]:
             result_df.loc[result_df["sample"] == sample_name, column] *= value2metric
         # Second adapt the columns from the sampling domain
+        factor = altitude2metric if convert_height else 1.0
         result_df.loc[result_df["sample"] == sample_name, "sample_domain"] = \
             result_df.loc[result_df["sample"] == sample_name, "sample_domain"].apply(
-                lambda val: np.fromstring(val.replace('[', '').replace(']', ''), sep=',') * altitude2metric
+                lambda val: np.fromstring(val.replace('[', '').replace(']', ''), sep=',') * factor
             )
+        # Rename columns of altitudes to real values
+        for column in [x for x in result_df.columns if "@Altitude_" in x]:
+            match = re.search(r"(.*@)Altitude_(\d+)", column) \
+                if not only_height else re.search(r"().*@Altitude_(\d+)", column)
+            if match is not None:
+                altitude_prefix = match.group(1)
+                altitude_id = int(match.group(2))
+                result_df.rename(columns={
+                    column: f"{altitude_prefix}{altitudes[altitude_id] * factor}"
+                }, inplace=True)
     return result_df
 
 
