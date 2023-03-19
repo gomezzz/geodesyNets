775b0e61a821af4436496fd0f9f667e854722328
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2023-03-20 00:34:33 +0100

    adds binding low_res mesh

--- gravann/labels/binder.py
+++ gravann/labels/binder.py
@@ -29,20 +29,21 @@ def bind_label(method: str, use_acc: bool = True, **kwargs) -> Callable[[torch.T
                                         Can also be a scalar containing the mass value for all points.
         mesh_vertices (2-D array-like): an (N, 3) array-like object containing the vertices of the polyhedron
         mesh_faces (2-D array-like): a (N,) array-like object containing the edges of the polyhedron
+        low_res (bool): loads the low resolution polyhedral mesh if given and set to true
 
     Returns:
         callable function taking an input tensor of points and returning the corresponding label's values
 
     """
-    sample = kwargs.get("sample", None)
     if method == 'mascon':
-        if sample is not None:
+        if sample := kwargs.get("sample", None):
             mascon_points, mascon_masses = sample_reader.load_mascon_data(sample)
         else:
             mascon_points, mascon_masses = kwargs.get("mascon_points", None), kwargs.get("mascon_masses", None)
         return lambda points: _METHOD_REGISTRY[(method, use_acc)](points, mascon_points, mascon_masses)
     elif method == 'polyhedral':
-        if sample is not None:
+        if sample := kwargs.get("sample", None):
+            sample = f"{sample}_lp" if kwargs.get("low_res", False) else sample
             mesh_vertices, mesh_faces = sample_reader.load_polyhedral_mesh(sample)
         else:
             mesh_vertices, mesh_faces = kwargs.get("mesh_vertices", None), kwargs.get("mesh_faces", None)
