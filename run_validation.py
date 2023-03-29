3f649cc16eb74452b935a9e3824e017e6c3ac68c
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2023-03-29 15:22:54 +0200

    adapt integration points

--- run_validation.py
+++ run_validation.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         output_directory,
         N=10000,
         sampling_altitudes=[0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0, 5.0, 10.0, 50.0],
-        integration_points=500000,
+        integration_points=300000,
         cuda_devices=cuda_devices,
         # with_constant_noise=True
     )
